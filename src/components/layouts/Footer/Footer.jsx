ad46d5c849403f6b9b71f5968ff026e73df26579
Ilya Bondarchuk <cod3provider@gmail.com>
2026-03-21 19:32:49 +0100

    add content to footer with styles

--- src/components/layouts/Footer/Footer.jsx
+++ src/components/layouts/Footer/Footer.jsx
@@ -1,10 +1,27 @@
 import Container from "@components/common/Container/Container.jsx";
+import Logo from "@components/common/Logo/Logo.jsx";
+
+import cl from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer>
       <Container>
-        <p>footer</p>
+        <div className={cl.footer}>
+          <Logo />
+          <ul className={cl.footerLinksList}>
+            <li>
+              <a className={cl.footerLink} href="">Privacy</a>
+            </li>
+            <li>
+              <a className={cl.footerLink} href="">Terms</a>
+            </li>
+            <li>
+              <a className={cl.footerLink} href="">Contact</a>
+            </li>
+          </ul>
+          <p className={cl.copyright}>&#169; 2026 DevQuest. Made with joy for all.</p>
+        </div>
       </Container>
     </footer>
   )
